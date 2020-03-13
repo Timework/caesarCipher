8b0bbd8e7e1bb692d7a09d14bddf3371f42c8266
Timework <timework.bibo@gmail.com>
2020-03-13 11:24:00 -0500

    finished cipher

---
+++ caesar.rb
@@ -0,0 +1,16 @@
+def caesar(words, move)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    words.each_char.with_index do |char, index|
+        if alphabet.include?(char.downcase)
+            if char.upcase == char
+                nIndex = alphabet.index(char.downcase) + move
+                words[index] = alphabet[nIndex % alphabet.length].upcase
+            else
+                nIndex = alphabet.index(char.downcase) + move
+                words[index] = alphabet[nIndex % alphabet.length]
+            end
+        end
+    end
+    puts words
+end
+caesar("What a string!", 5)
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is a caesar cipher for project 1 of Ruby Programming Project:Building Blocks.
+completed :03/13/2020
